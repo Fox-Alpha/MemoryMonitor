b6a75607d99e1cdd1f17b0c295fa47fa9530111a
Dirk.Buck <dirk.buck@alpha-com.de>
2016-01-05 08:59:32 +0100

    Add: erstellen einer Form zum anzeigen der Überwachungsinformtion

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: buck
+ * Datum: 05.01.2016
+ * Zeit: 08:46
+ * 
+ */
+namespace MemoryMonitor.Forms
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(311, 124);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Überwacht den Speicherverbrauch einer Anwendung und erstellt einen Screenshot bei" +
+	" überschreitung eines bestimmten Wertes";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(357, 194);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "MainForm";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Forms/MainForm.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: buck
+ * Datum: 05.01.2016
+ * Zeit: 08:46
+ * 
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MemoryMonitor.Forms
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
