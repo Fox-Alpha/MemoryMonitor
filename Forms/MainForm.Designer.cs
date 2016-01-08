4cb962439be5a13318fd6f5fdef0b554a5a8dd09
Dirk.Buck <dirk.buck@alpha-com.de>
2016-01-08 11:43:45 +0100

    Add: Hauptmenü und Statusleiste dem Form hinzugefügt

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -14,6 +14,13 @@ namespace MemoryMonitor.Forms
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem programmToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem optionenToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem überwachungToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem einstellungenToolStripMenuItem;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -37,6 +44,14 @@ namespace MemoryMonitor.Forms
 		private void InitializeComponent()
 		{
 			this.label1 = new System.Windows.Forms.Label();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.programmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.optionenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.überwachungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.einstellungenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -49,15 +64,77 @@ namespace MemoryMonitor.Forms
 			this.label1.Text = "Überwacht den Speicherverbrauch einer Anwendung und erstellt einen Screenshot bei" +
 	" überschreitung eines bestimmten Wertes";
 			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 172);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(357, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.programmToolStripMenuItem,
+			this.optionenToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(357, 24);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// programmToolStripMenuItem
+			// 
+			this.programmToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.beendenToolStripMenuItem});
+			this.programmToolStripMenuItem.Name = "programmToolStripMenuItem";
+			this.programmToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+			this.programmToolStripMenuItem.Text = "Programm";
+			// 
+			// beendenToolStripMenuItem
+			// 
+			this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+			this.beendenToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+			this.beendenToolStripMenuItem.Text = "Beenden";
+			// 
+			// optionenToolStripMenuItem
+			// 
+			this.optionenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.überwachungToolStripMenuItem,
+			this.einstellungenToolStripMenuItem});
+			this.optionenToolStripMenuItem.Name = "optionenToolStripMenuItem";
+			this.optionenToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+			this.optionenToolStripMenuItem.Text = "Optionen";
+			// 
+			// überwachungToolStripMenuItem
+			// 
+			this.überwachungToolStripMenuItem.Name = "überwachungToolStripMenuItem";
+			this.überwachungToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+			this.überwachungToolStripMenuItem.Text = "Überwachung";
+			// 
+			// einstellungenToolStripMenuItem
+			// 
+			this.einstellungenToolStripMenuItem.Name = "einstellungenToolStripMenuItem";
+			this.einstellungenToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+			this.einstellungenToolStripMenuItem.Text = "Einstellungen";
+			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(357, 194);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.menuStrip1);
 			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "MainForm";
+			this.ShowIcon = false;
 			this.Text = "MainForm";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 	}
